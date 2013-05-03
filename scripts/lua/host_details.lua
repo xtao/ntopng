a6541eb2d651a79f2e521eed8633b3d63e6237b0
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2013-05-03 16:31:37 +0000

    Minor change
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@6215 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/lua/host_details.lua
+++ scripts/lua/host_details.lua
@@ -2,6 +2,9 @@ ntop.dumpFile("./httpdocs/inc/header.inc")
 
 ntop.dumpFile("./httpdocs/inc/menu.inc")
 
+page = _GET["page"]
+
+
 print [[
 
 
@@ -10,11 +13,43 @@ print [[
 ]]
 
 
-print("<li>".._GET["host"].."</li>"	)
+print("<li>".._GET["host"].."</li>\n"	)
 
 print [[
 </ul>
 
+
+<div class="bs-docs-example">
+            <div class="navbar">
+              <div class="navbar-inner">
+<ul class="nav">
+]]
+
+url="/host_details.lua?host=".._GET["host"]
+
+if((page == "overview") or (page == nil)) then
+  print("<li class=\"active\"><a href=\"#\">Host Overview</a></li>\n")
+else
+  print("<li><a href=\""..url.."&page=overview\">Host Overview</a></li>")
+end
+
+if(page == "flows") then
+  print("<li class=\"active\"><a href=\"#\">Active Flows</a></li>\n")
+else
+  print("<li><a href=\""..url.."&page=flows\">Active Flows</a></li>")
+end
+
+if(page == "historical") then
+  print("<li class=\"active\"><a href=\"#\">Historical Activity</a></li>\n")
+else
+  print("<li><a href=\""..url.."&page=historical\">Historical Activity</a></li>")
+end
+
+print [[
+</ul>
+</div>
+            </div>
+          </div>
 
 Hello
 
