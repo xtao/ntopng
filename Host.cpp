4af9613589d2d6e4a92b20efa566218641c1b30d
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2013-05-30 22:33:12 +0000

    Crash fix
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@6326 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- Host.cpp
+++ Host.cpp
@@ -63,6 +63,7 @@ void Host::initialize(u_int8_t mac[6], u_int16_t _vlanId, bool init_all) {
   num_uses = 0, name_resolved = false, symbolic_name = NULL, vlan_id = _vlanId;
   first_seen = last_seen = iface->getTimeLastPktRcvd();
   m = new Mutex();
+  localHost = false, asn = 0, asname = NULL, country = NULL, city = NULL;
 
   if(init_all) {
     char buf[64], rsp[256], *host = ip->print(buf, sizeof(buf));
@@ -76,8 +77,7 @@ void Host::initialize(u_int8_t mac[6], u_int16_t _vlanId, bool init_all) {
     ntop->getGeolocation()->getInfo(ip, &country, &city, &latitude, &longitude);
     
     updateLocal();
-  } else
-    localHost = false, asn = 0, asname = NULL, country = NULL, city = NULL;
+  }
 }
 
 /* *************************************** */
