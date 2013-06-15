cb242098d1da2c1ae7543c3d30778f903479198b
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2013-06-15 13:27:17 +0000

    Win32 initial files
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@6420 a02cd0c1-8e76-42e1-a119-56f9641475e2

---
+++ ntop_win32.h
@@ -0,0 +1,48 @@
+/*
+ *
+ * (C) 2013 - ntop.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef _NTOP_WIN32_H_
+#define _NTOP_WIN32_H_
+
+
+#ifdef WIN32
+#include <winsock2.h> /* winsock.h is included automatically */
+#include <process.h>
+#include <io.h>
+#include <getopt.h> /* getopt from: http://www.pwilson.net/sample.html. */
+#include <process.h> /* for getpid() and the exec..() family */
+
+#define srandom srand
+#define random rand
+
+/* Values for the second argument to access. These may be OR'd together.  */
+#define R_OK    4       /* Test for read permission.  */
+#define W_OK    2       /* Test for write permission.  */
+//#define   X_OK    1       /* execute permission - unsupported in windows*/
+#define F_OK    0       /* Test for existence.  */
+
+#define access _access
+#define ftruncate _chsize
+
+#include "dirent.h"
+#endif
+
+#endif /* _NTOP_WIN32_H_ */
