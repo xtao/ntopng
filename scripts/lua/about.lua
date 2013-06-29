6c19076857b5ae7ad70d9ab161fed7d51ed7d59f
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2013-06-29 17:07:40 +0000

    Minor change
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@6527 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/lua/about.lua
+++ scripts/lua/about.lua
@@ -21,7 +21,8 @@ print [[
 print("<table class=\"table table-bordered table-striped\">\n")
 print("<tr><th>Copyright</th><td>"..info["copyright"].."</td></tr>\n")
 print("<tr><th>Authors</th><td>"..info["authors"].."</td></tr>\n")
-print("<tr><th>License</th><td><A HREF=http://www.gnu.org/licenses/gpl.html>"..info["license"].."</A></td></tr>\n")
+print("<tr><th>License</th><td><A HREF=http://www.gnu.org/licenses/gpl.html>"..info["license"].."</A>")
+
 print("<tr><th>Version</th><td>"..info["version"].."</td></tr>\n")
 print("<tr><th>Currently Logged User</th><td>".._SESSION["user"].."</td></tr>\n")
 print("<tr><th>Uptime</th><td>"..secondsToTime(info["uptime"]).."</td></tr>\n")
@@ -35,8 +36,10 @@ print("<tr><th><a href=http://www.luajit.org>LuaJIT</A></th><td>"..info["version
 print("<tr><th><a href=http://www.zeromq.org>ØMQ</A></th><td>"..info["version.zmq"].."</td></tr>\n")
 print("<tr><th><a href=http://www.maxmind.com>GeoIP</A></th><td>"..info["version.geoip"])
 
-print [[ <br>This product includes GeoLite data created by MaxMind,<br>available from
-	 <a href="http://www.maxmind.com">http://www.maxmind.com</a>. ]]
+print [[ <p><small>
+         <div class="alert alert-info">This product includes GeoLite data created by MaxMind, available from
+	 <a href="http://www.maxmind.com">http://www.maxmind.com</a>. </div></small>
+]]
 
 print("</td></tr>\n")
 print("<tr><th><a href=http://www.d3js.org>Data-Driven Documents (d3js)</A></th><td>2.9.1 / 3.0</td></tr>\n")
