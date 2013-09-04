910fa6b4214387b98f6a181e1bdcf05629325b79
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2013-09-04 15:49:05 +0000

    Missing header
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@6754 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/lua/get_host_activitymap.lua
+++ scripts/lua/get_host_activitymap.lua
@@ -10,7 +10,9 @@ require "graph_utils"
 
 host_ip = _GET["host"]
 
+sendHTTPHeader('application/json')
+
 interface.find(ifname)
 rsp = interface.getHostActivityMap(host_ip)
-
+--print (host_ip)
 print(rsp)
