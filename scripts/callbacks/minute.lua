4a882d2d362357781afce97cc7193afe94d1df87
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2013-11-13 06:22:03 +0000

    Patch courtesy of Steve Clark <sclark@netwolves.com>
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@6959 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/callbacks/minute.lua
+++ scripts/callbacks/minute.lua
@@ -42,51 +42,78 @@ for _,_ifname in pairs(ifnames) do
 
    -- io.write('Diff: '..diff..'\n')
    
-   if(diff > 59) then
-      return
-   end
-
-   interface.find(_ifname)
-   hosts_stats = interface.getHostsInfo()
-   for key, value in pairs(hosts_stats) do
-      if(verbose == 1) then print ("[" .. key .. "][" .. (hosts_stats[key]["bytes.sent"]+hosts_stats[key]["bytes.rcvd"]) .. "]\n") end
-
-      if(hosts_stats[key]["localhost"] == true) then
-	 basedir = fixPath(dirs.workingdir .. "/" .. ifname .. "/rrd/" .. key)
-
-	 if(not(ntop.exists(basedir))) then
-	    if(verbose == 1) then io.write('Creating base directory ', basedir, '\n') end
-	    ntop.mkdir(basedir)
-	 end
-
-	 -- Traffic stats
-	 name = fixPath(basedir .. "/bytes.rrd")
-	 if(not(ntop.exists(name))) then
-	    if(verbose == 1) then io.write('Creating RRD ', name, '\n') end
-	    ntop.rrd_create(
-	       name,
-	       '--start', 'now',
-	       '--step', '300',
-	       'DS:sent:DERIVE:600:U:U',
-	       'DS:rcvd:DERIVE:600:U:U',
-	       'RRA:AVERAGE:0.5:1:7200',  -- raw: 1 day = 1 * 24 = 24 * 300 sec = 7200
-	       'RRA:AVERAGE:0.5:12:2400',  -- 1h resolution (12 points)   2400 hours = 100 days
-	       'RRA:AVERAGE:0.5:288:365',  -- 1d resolution (288 points)  365 days
-	       'RRA:HWPREDICT:1440:0.1:0.0035:20')
+   if(diff < 60) then
+      
+      interface.find(_ifname)
+      hosts_stats = interface.getHostsInfo()
+      for key, value in pairs(hosts_stats) do
+	 if(verbose == 1) then print ("[" .. key .. "][" .. (hosts_stats[key]["bytes.sent"]+hosts_stats[key]["bytes.rcvd"]) .. "]\n") end
+	 
+	 if(hosts_stats[key]["localhost"] == true) then
+	    basedir = fixPath(dirs.workingdir .. "/" .. ifname .. "/rrd/" .. key)
 	    
-	 end
-	 ntop.rrd_update(name, "N:"..hosts_stats[key]["bytes.sent"] .. ":" .. hosts_stats[key]["bytes.rcvd"])
-	 if(verbose == 1) then io.write('Updating RRD '..name..'\n') end
-
-	 -- L4 Protocols
-	 host = interface.getHostInfo(key)
-	 if((host ~= nil) and host.localhost) then	 
-	    for id, _ in ipairs(l4_keys) do
-	       k = l4_keys[id][2]
-
-	       if((host[k..".bytes.sent"] ~= nil) and (host[k..".bytes.rcvd"] ~= nil)) then
-		  if(verbose == 1) then print("\t"..k.."\n") end
+	    if(not(ntop.exists(basedir))) then
+	       if(verbose == 1) then io.write('Creating base directory ', basedir, '\n') end
+	       ntop.mkdir(basedir)
+	    end
+	    
+	    -- Traffic stats
+	    name = fixPath(basedir .. "/bytes.rrd")
+	    if(not(ntop.exists(name))) then
+	       if(verbose == 1) then io.write('Creating RRD ', name, '\n') end
+	       ntop.rrd_create(
+		  name,
+		  '--start', 'now',
+		  '--step', '300',
+		  'DS:sent:DERIVE:600:U:U',
+		  'DS:rcvd:DERIVE:600:U:U',
+		  'RRA:AVERAGE:0.5:1:7200',  -- raw: 1 day = 1 * 24 = 24 * 300 sec = 7200
+		  'RRA:AVERAGE:0.5:12:2400',  -- 1h resolution (12 points)   2400 hours = 100 days
+		  'RRA:AVERAGE:0.5:288:365',  -- 1d resolution (288 points)  365 days
+		  'RRA:HWPREDICT:1440:0.1:0.0035:20')
+	       
+	    end
+	    ntop.rrd_update(name, "N:"..hosts_stats[key]["bytes.sent"] .. ":" .. hosts_stats[key]["bytes.rcvd"])
+	    if(verbose == 1) then io.write('Updating RRD '..name..'\n') end
+	    
+	    -- L4 Protocols
+	    host = interface.getHostInfo(key)
+	    if((host ~= nil) and host.localhost) then	 
+	       for id, _ in ipairs(l4_keys) do
+		  k = l4_keys[id][2]
 		  
+		  if((host[k..".bytes.sent"] ~= nil) and (host[k..".bytes.rcvd"] ~= nil)) then
+		     if(verbose == 1) then print("\t"..k.."\n") end
+		     
+		     name = fixPath(basedir .. "/".. k .. ".rrd")
+		     if(not(ntop.exists(name))) then
+			if(verbose == 1) then io.write('Creating RRD ', name, '\n') end
+			ntop.rrd_create(
+			   name,
+			   '--start', 'now',
+			   '--step', '300',
+			   'DS:sent:DERIVE:600:U:U',
+			   'DS:rcvd:DERIVE:600:U:U',
+			   'RRA:AVERAGE:0.5:1:7200',  -- raw: 1 day = 1 * 24 = 24 * 300 sec = 7200
+			   'RRA:AVERAGE:0.5:12:2400',  -- 1h resolution (12 points)   2400 hours = 100 days
+			   'RRA:AVERAGE:0.5:288:365',  -- 1d resolution (288 points)  365 days
+			   'RRA:HWPREDICT:1440:0.1:0.0035:20')
+		     end
+		     
+		     -- io.write(name.."="..host[k..".bytes.sent"].."|".. host[k..".bytes.rcvd"] .. "\n")
+		     ntop.rrd_update(name, "N:".. host[k..".bytes.sent"] .. ":" .. host[k..".bytes.rcvd"])
+		     if(verbose == 1) then io.write('Updating RRD '..name..'\n') end
+		  else
+		     -- L2 host
+		     --io.write("Discarding "..k.."@"..key.."\n")
+		  end
+	       end
+	    end
+	    
+	    -- nDPI Protocols
+	    host = interface.getHostInfo(key)
+	    if((host ~= nil) and host.localhost) then
+	       for k in pairs(host["ndpi"]) do
 		  name = fixPath(basedir .. "/".. k .. ".rrd")
 		  if(not(ntop.exists(name))) then
 		     if(verbose == 1) then io.write('Creating RRD ', name, '\n') end
@@ -102,41 +129,13 @@ for _,_ifname in pairs(ifnames) do
 			'RRA:HWPREDICT:1440:0.1:0.0035:20')
 		  end
 		  
-		  -- io.write(name.."="..host[k..".bytes.sent"].."|".. host[k..".bytes.rcvd"] .. "\n")
-		  ntop.rrd_update(name, "N:".. host[k..".bytes.sent"] .. ":" .. host[k..".bytes.rcvd"])
+		  ntop.rrd_update(name, "N:".. host["ndpi"][k]["bytes.sent"] .. ":" .. host["ndpi"][k]["bytes.rcvd"])
 		  if(verbose == 1) then io.write('Updating RRD '..name..'\n') end
-	       else
-		  -- L2 host
-		  --io.write("Discarding "..k.."@"..key.."\n")
 	       end
 	    end
-	 end
-
-	 -- nDPI Protocols
-	 host = interface.getHostInfo(key)
-	 if((host ~= nil) and host.localhost) then
-	    for k in pairs(host["ndpi"]) do
-	       name = fixPath(basedir .. "/".. k .. ".rrd")
-	       if(not(ntop.exists(name))) then
-		  if(verbose == 1) then io.write('Creating RRD ', name, '\n') end
-		  ntop.rrd_create(
-		     name,
-		     '--start', 'now',
-		     '--step', '300',
-		     'DS:sent:DERIVE:600:U:U',
-		     'DS:rcvd:DERIVE:600:U:U',
-		     'RRA:AVERAGE:0.5:1:7200',  -- raw: 1 day = 1 * 24 = 24 * 300 sec = 7200
-		     'RRA:AVERAGE:0.5:12:2400',  -- 1h resolution (12 points)   2400 hours = 100 days
-		     'RRA:AVERAGE:0.5:288:365',  -- 1d resolution (288 points)  365 days
-		     'RRA:HWPREDICT:1440:0.1:0.0035:20')
-	       end
-
-	       ntop.rrd_update(name, "N:".. host["ndpi"][k]["bytes.sent"] .. ":" .. host["ndpi"][k]["bytes.rcvd"])
-	       if(verbose == 1) then io.write('Updating RRD '..name..'\n') end
-	    end
-	 end
-
-      end -- if
-   end -- for
+	    
+	 end -- if
+      end -- for
+   end -- if(diff
 
 end -- for ifname,_ in pairs(ifnames) do
