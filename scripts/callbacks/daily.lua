73f8701968965faa178bc2951b4889459fd2b499
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2013-12-02 22:48:31 +0000

    Data is now dumped on sqlite
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@7063 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/callbacks/daily.lua
+++ scripts/callbacks/daily.lua
@@ -8,22 +8,50 @@ package.path = dirs.installdir .. "/scripts/lua/modules/?.lua;" .. package.path
 
 require "lua_utils"
 
-local debug = true
-local delete_keys = true
+local debug = false
+local delete_keys = false
+
+begin = os.clock()
+t = os.time() -86400
+when = os.date("%y%m%d", t)
+key_name = when..".keys"
+
+--print(key_name.."\n")
+
+dump_dir = fixPath(dirs.workingdir .. "/datadump/")
+ntop.mkdir(dump_dir)
+
+db =  sqlite3.open(dump_dir.."20"..when..".sqlite")
+db:exec[[
+      CREATE TABLE IF NOT EXISTS `interfaces` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `interface_name` STRING);
+      CREATE TABLE IF NOT EXISTS `hosts` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `host_name` STRING);
+      CREATE TABLE IF NOT EXISTS `activities` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `interface_idx` INTEGER, `host_idx` INTEGER, `type` INTEGER);
+      CREATE TABLE IF NOT EXISTS `contacts` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `activity_idx` INTEGER KEY, `contact_type` INTEGER, `host_idx` INTEGER KEY, `contact_family` INTEGER, `num_contacts` INTEGER);
+]]
+
+db:exec('INSERT INTO contacts(idx, activity_idx, contact_type, host_idx, contact_family, num_contacts) VALUES (339,0,0,1,65535,34);')
+db:exec('INSERT INTO activities(idx, interface_idx, host_idx, type) VALUES (213,0,156,0);')
+
+-- #########################
 
 function cleanName(name)
    n = string.gsub(name, "'", "_")
+   n = string.gsub(n, ",", "_")
    -- Cut the name at 128 chars
    n = string.sub(n, 1, 128)
    return(n)
 end
 
+-- #########################  
+
 interfaces_id     = 0
 interfaces_hash   = { }
+-- Interfaces are a few so we still cache them
 function interface2id(name)
    if(interfaces_hash[name] == nil) then
       id = interfaces_id
       interfaces_hash[name] = id
+      db:exec('INSERT INTO interfaces(idx, interface_name) VALUES ('..id..', "'.. name .. '");')
       interfaces_id = interfaces_id + 1
       return(id)
    else
@@ -33,71 +61,46 @@ end
 
 -- #########################
 
-hosts_id     = 0
-hosts_hash   = { }
+hosts_id = 0
 function host2id(name)
    name = cleanName(name)
-   if(hosts_hash[name] == nil) then
-      id = hosts_id
-      hosts_hash[name] = id
-      hosts_id = hosts_id + 1
-      return(id)
-   else
-      return(hosts_hash[name])
-   end
+
+   for idx in db:urows('SELECT idx from hosts where host_name="'..name..'"')
+   do return(idx) end
+
+   db:exec('INSERT INTO hosts(idx, host_name) VALUES ('..hosts_id..', "'.. name .. '");')
+   hosts_id = hosts_id + 1
+   return(hosts_id)
+end
+
+-- #########################
+
+function add_to_activities(a, b, c, d)
+   sql = 'INSERT INTO activities(idx, interface_idx, host_idx, type) VALUES ('..a..','..b..','..c..','..d..');'
+   if(debug) then print(sql.."\n") end
+   db:exec(sql)
+end
+
+-- #########################
+
+function add_to_contacts(a, b, c, d, e, f)
+   sql = 'INSERT INTO contacts(idx, activity_idx, contact_type, host_idx, contact_family, num_contacts) VALUES ('..a..','..b..','..c..','..d..','..e..','..f..');'
+   if(debug) then print(sql.."\n") end
+   db:exec(sql)
 end
 
 -- #########################
 
 if(debug) then sendHTTPHeader('text/html') end
 
-begin = os.clock()
-t = os.time()-86400
-when = os.date("%y%m%d", t)
-key_name = when..".keys"
-
---print(key_name.."\n")
-
-dump_dir = fixPath(dirs.workingdir .. "/datadump/")
-ntop.mkdir(dump_dir)
-
-fname = dump_dir .. "20".. when
-activities_name = fname  .."_activities.csv"
-contacts_name = fname  .."_contacts.csv"
-
-hosts_name = fname  .."_hosts.csv"
-interfaces_name = fname  .."_interfaces.csv"
-
-tables_name = fname  ..".sql"
-
-activities = io.open(activities_name, "w")
-contacts = io.open(contacts_name, "w")
-hosts = io.open(hosts_name, "w")
-interfaces = io.open(interfaces_name, "w")
-tables = io.open(tables_name, "w")
-
-print("Please wait: we are dumping data into<ul>\n")
-print("<li>"..activities_name.."\n")
-print("<li>"..contacts_name.."\n")
-print("<li>"..hosts_name.."\n")
-print("<li>"..interfaces_name.."\n")
-print("<li>"..tables_name.."\n")
-
-print("</ul>\n")
-
-interfaces:write("# idx | interface\n")
-hosts:write("# idx | name\n")
-activities:write("# host_contacts=0, aggregations=1\n")
-contacts:write("# contacted_by=0, contacted_peers=1\n")
-
 contact_idx = 0
 idx = 0
 repeat
    key = ntop.setPopCache(key_name)
-   if(debug) then print("====> "..key_name.."<br>\n") end
+   if(debug) then print("====> "..key_name.."\n") end
    if((key == nil) or (key == "")) then break end
 
-   if(debug) then print("=> "..key.."<br>\n") end
+   if(debug) then print("=> "..key.."\n") end
    k1 = when.."|"..key.."|contacted_by"
    v1 = ntop.getHashKeysCache(k1)
    if(v1 ~= nil) then
@@ -110,7 +113,7 @@ repeat
 	 r = 1 
       end
       name = host2id(res[4])
-      activities:write(idx..",".. interface2id(res[3])..","..name..",".. r.."\n")
+      add_to_activities(idx,interface2id(res[3]),name,r)
 
       if(debug) then print("-> (1)"..k1.."\n") end
       for k,_ in pairs(v1) do
@@ -118,7 +121,7 @@ repeat
 	 res = split(k, "@")
 	 if(debug) then print("\t"..k .. "=" .. v.. "\n") end
 	 if((res[1] ~= nil) and (res[2] ~= nil) and (v ~= nil)) then
-	    contacts:write(contact_idx..","..idx..",0,"..host2id(res[1])..",".. res[2]..",".. v.."\n")
+	    add_to_contacts(contact_idx,id,0,host2id(res[1]),res[2],v)
 	    contact_idx = contact_idx + 1
 	 end
 	 if(delete_keys) then ntop.delHashCache(k1, k) end
@@ -139,7 +142,7 @@ repeat
 	 r = 1 
       end
       name = host2id(res[4])
-      activities:write(idx..",".. interface2id(res[3])..",".. name ..",".. r .."\n")
+      add_to_activities(idx,interface2id(res[3]),name,r)
 
       if(debug) then print("-> (2)"..k2.."\n") end
       for k,v in pairs(v2) do
@@ -147,7 +150,7 @@ repeat
 	 res = split(k, "@")
 	 if(debug) then print("\t"..k .. "=" .. v.. "\n") end
 	 if((res[1] ~= nil) and (res[2] ~= nil) and (v ~= nil)) then
-	    contacts:write(contact_idx..","..idx..",1,"..host2id(res[1])..",".. res[2]..",".. v.."\n")
+	    add_to_contacts(contact_idx,idx,1,host2id(res[1]),res[2],v)
 	    contact_idx = contact_idx + 1
 	 end
 	 if(delete_keys) then ntop.delHashCache(k2, k) end
@@ -158,41 +161,10 @@ repeat
 
    until(key == "")
 
--- Dump Interfaces
-print(interfaces_id.." interfaces found<br>\n")
-for iface,id in pairs(interfaces_hash) do
-   interfaces:write(id..","..iface.."\n")
-end
-
-
--- Dump Hosts
-print(hosts_id.." hosts found<br>\n")
-for host,id in pairs(hosts_hash) do
-   hosts:write(id..","..host.."\n")
-end
-
-contacts:close()
-activities:close()
-hosts:close()
-
--- #########################################################
-
-tables:write("CREATE DATABASE IF NOT EXISTS `20"..when.."`;\nUSE `20"..when.."`;\n\n")
-tables:write("\nCREATE TABLE IF NOT EXISTS `interfaces` (\n`idx` int(11) NOT NULL,\n`interface_name` VARCHAR(32) NOT NULL,\nPRIMARY KEY (`idx`)\n) ENGINE=InnoDB DEFAULT CHARSET=latin1;\n\n")
-tables:write("\nCREATE TABLE IF NOT EXISTS `hosts` (\n`idx` int(11) NOT NULL,\n`host_name` VARCHAR(128) NOT NULL,\nPRIMARY KEY (`idx`)\n) ENGINE=InnoDB DEFAULT CHARSET=latin1;\n\n")
-tables:write("\nCREATE TABLE IF NOT EXISTS `activities` (\n`idx` int(11) NOT NULL,\n`interface_idx` int(11) NOT NULL,\n`host_idx` int(11) NOT NULL,\n`type` INT(8) NOT NULL,\nPRIMARY KEY (`idx`)\n) ENGINE=InnoDB DEFAULT CHARSET=latin1;\n\n")
-tables:write("\nCREATE TABLE IF NOT EXISTS `contacts` (\n`idx` int(11) NOT NULL,\n`activity_idx` int(11) NOT NULL,\n`contact_type` int(11) NOT NULL,\n`host_idx` int(11) NOT NULL,\n`contact_family` int(8) DEFAULT NULL,\n`num_contacts` int(8) NOT NULL,\nPRIMARY KEY (`idx`)\n) ENGINE=InnoDB DEFAULT CHARSET=latin1;\n\n")
-
-tables:write('mysql --local-infile=1 -u root -p<pass> 20'..when..'\n')
-tables:write('LOAD data local infile "/var/tmp/ntopng/datadump/20'..when..'_interfaces.csv" INTO TABLE interfaces fields terminated by "," ignore 1 lines;\n')
-tables:write('LOAD data local infile "/var/tmp/ntopng/datadump/20'..when..'_hosts.csv" INTO TABLE hosts fields terminated by "," ignore 1 lines;\n')
-tables:write('LOAD data local infile "/var/tmp/ntopng/datadump/20'..when..'_activities.csv" INTO TABLE activities fields terminated by "," ignore 1 lines;\n')
-tables:write('LOAD data local infile "/var/tmp/ntopng/datadump/20'..when..'_contacts.csv" INTO TABLE contacts fields terminated by "," ignore 1 lines;\n')
-
-tables:close()
+db:close()
 
 sec = os.clock() - begin
-print(string.format("Elapsed time: %.2f min\n", sec/60).."<br>\n")
+print(string.format("Elapsed time: %.2f min\n", sec/60).."\n")
 print("\nDone.\n")
 
 -- redis-cli KEYS "131129|*" | xargs redis-cli DEL
