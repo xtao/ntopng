48c2993a84377d8e9e48784e138c47e3c0ee3f44
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2013-12-03 13:50:38 +0000

    Minor change
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@7066 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/callbacks/daily.lua
+++ scripts/callbacks/daily.lua
@@ -12,7 +12,7 @@ local debug = false
 local delete_keys = true
 
 begin = os.clock()
-t = os.time() -86400
+t = os.time()-86400
 
 if((_GET ~= nil) and (_GET["debug"] ~= nil)) then
    debug = true
@@ -29,18 +29,15 @@ key_name = when..".keys"
 dump_dir = fixPath(dirs.workingdir .. "/datadump/")
 ntop.mkdir(dump_dir)
 
-db =  sqlite3.open(dump_dir.."20"..when..".sqlite")
-mem_db = sqlite3.open_memory()
+db_name = dump_dir.."20"..when..".sqlite"
+print("Opening DB: "..db_name.."\n")
+db =  sqlite3.open(db_name)
 
 db:exec[[
       CREATE TABLE IF NOT EXISTS `interfaces` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `interface_name` STRING);
       CREATE TABLE IF NOT EXISTS `hosts` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `host_name` STRING KEY);
       CREATE TABLE IF NOT EXISTS `activities` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `interface_idx` INTEGER, `host_idx` INTEGER, `type` INTEGER);
       CREATE TABLE IF NOT EXISTS `contacts` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `activity_idx` INTEGER KEY, `contact_type` INTEGER, `host_idx` INTEGER KEY, `contact_family` INTEGER, `num_contacts` INTEGER);
-]]
-
-mem_db:exec[[
-    CREATE TABLE IF NOT EXISTS `hosts` (`idx` INTEGER PRIMARY KEY AUTOINCREMENT, `host_name` STRING KEY);
 ]]
 
 -- #########################
@@ -54,8 +51,10 @@ end
 -- #########################
 
 function cleanName(name)
+   -- print(name.."\n")
    n = string.gsub(name, "'", "_")
    n = string.gsub(n, ",", "_")
+   n = string.gsub(n, '"', "_")
    -- Cut the name at 128 chars
    n = string.sub(n, 1, 128)
    return(n)
@@ -85,11 +84,10 @@ host_id = 0
 function host2id(name)
    name = cleanName(name)
 
-   for idx in mem_db:urows('SELECT idx FROM hosts WHERE host_name="'..name..'"')
+   for idx in db:urows('SELECT idx FROM hosts WHERE host_name="'..name..'"')
    do return(idx) end
 
    execQuery(db, 'INSERT INTO hosts(idx, host_name) VALUES ('..host_id..', "'.. name .. '");')
-   execQuery(mem_db, 'INSERT INTO hosts(idx, host_name) VALUES ('..host_id..', "'.. name .. '");')
    host_id = host_id + 1
    return(host_id)
 end
@@ -98,7 +96,6 @@ end
 
 function add_to_activities(a, b, c, d)
    sql = 'INSERT INTO activities(idx, interface_idx, host_idx, type) VALUES ('..a..','..b..','..c..','..d..');'
-   if(debug) then print(sql.."\n") end
    execQuery(db, sql)
 end
 
@@ -106,7 +103,6 @@ end
 
 function add_to_contacts(a, b, c, d, e, f)
    sql = 'INSERT INTO contacts(idx, activity_idx, contact_type, host_idx, contact_family, num_contacts) VALUES ('..a..','..b..','..c..','..d..','..e..','..f..');'
-   if(debug) then print(sql.."\n") end
    execQuery(db, sql)
 end
 
@@ -116,7 +112,7 @@ contact_id = 0
 activity_id = 0
 repeat
    key = ntop.setPopCache(key_name)
-   if(debug) then print("====> "..key_name.."\n") end
+   -- if(debug) then print("====> "..key_name.."\n") end
    if((key == nil) or (key == "")) then break end
 
    if(debug) then print("=> "..key.."\n") end
@@ -124,29 +120,38 @@ repeat
    v1 = ntop.getHashKeysCache(k1)
    if(v1 ~= nil) then
       res = split(k1, "|")
-
-      if(res[2] == "host_contacts") then 
-	 r = 0 
+      
+      if(#res ~= 5) then
+	 print("Invalid string ["..k1.."]")
       else
-	 -- aggregations
-	 r = 1 
-      end
-      name = host2id(res[4])
-      add_to_activities(activity_id,interface2id(res[3]),name,r)
-
-      if(debug) then print("-> (1)"..k1.."\n") end
-      for k,_ in pairs(v1) do
-	 v = ntop.getHashCache(k1, k)
-	 res = split(k, "@")
-	 if(debug) then print("\t"..k .. "=" .. v.. "\n") end
-	 if((res[1] ~= nil) and (res[2] ~= nil) and (v ~= nil)) then
-	    add_to_contacts(contact_id,id,0,host2id(res[1]),res[2],v)
-	    contact_id = contact_id + 1
+	 if(res[2] == "host_contacts") then 
+	    r = 0 
+	 else
+	    -- aggregations
+	    r = 1 
 	 end
-	 if(delete_keys) then ntop.delHashCache(k1, k) end
-      end
+	 name = host2id(res[4])
+	 add_to_activities(activity_id,interface2id(res[3]),name,r)
+	 
+	 if(debug) then print("-> (1)"..k1.."\n") end
+	 for k,_ in pairs(v1) do
+	    v = ntop.getHashCache(k1, k)
+	    res = split(k, "@")
 
-      activity_id = activity_id + 1
+	    if(#res ~= 2) then
+	       print("Invalid string ["..k.."]")
+	    else
+	       if(debug) then print("\t"..k .. "=" .. v.. "\n") end
+	       if((res[1] ~= nil) and (res[2] ~= nil) and (v ~= nil)) then
+		  add_to_contacts(contact_id,id,0,host2id(res[1]),res[2],v)
+		  contact_id = contact_id + 1
+	       end
+	       if(delete_keys) then ntop.delHashCache(k1, k) end
+	    end
+	 end
+	 
+	 activity_id = activity_id + 1
+      end
    end
 
    k2 = when.."|"..key.."|contacted_peers"
@@ -154,34 +159,42 @@ repeat
    if(v2 ~= nil) then
       res = split(k1, "|")
 
-      if(res[2] == "host_contacts") then 
-	 r = 0 
+      if(#res ~= 5) then
+	 print("Invalid string ["..k1.."]")
       else
-	 -- aggregations
-	 r = 1 
-      end
-      name = host2id(res[4])
-      add_to_activities(activity_id,interface2id(res[3]),name,r)
-
-      if(debug) then print("-> (2)"..k2.."\n") end
-      for k,v in pairs(v2) do
-	 v = ntop.getHashCache(k2, k)
-	 res = split(k, "@")
-	 if(debug) then print("\t"..k .. "=" .. v.. "\n") end
-	 if((res[1] ~= nil) and (res[2] ~= nil) and (v ~= nil)) then
-	    add_to_contacts(contact_id,activity_id,1,host2id(res[1]),res[2],v)
-	    contact_id = contact_id + 1
+	 if(res[2] == "host_contacts") then 
+	    r = 0 
+	 else
+	    -- aggregations
+	    r = 1 
 	 end
-	 if(delete_keys) then ntop.delHashCache(k2, k) end
-      end
+	 name = host2id(res[4])
+	 add_to_activities(activity_id,interface2id(res[3]),name,r)
 
-      activity_id = activity_id + 1
+	 if(debug) then print("-> (2)"..k2.."\n") end
+	 for k,v in pairs(v2) do
+	    v = ntop.getHashCache(k2, k)
+	    res = split(k, "@")
+
+	    if(#res ~= 2) then
+	       print("Invalid string ["..k.."]")
+	    else
+	       if(debug) then print("\t"..k .. "=" .. v.. "\n") end
+	       if((res[1] ~= nil) and (res[2] ~= nil) and (v ~= nil)) then
+		  add_to_contacts(contact_id,activity_id,1,host2id(res[1]),res[2],v)
+		  contact_id = contact_id + 1
+	       end
+	       if(delete_keys) then ntop.delHashCache(k2, k) end
+	    end
+	 end
+
+	 activity_id = activity_id + 1
+      end
    end
 
    until(key == "")
 
 db:close()
-mem_db:close()
 
 sec = os.clock() - begin
 print(string.format("Elapsed time: %.2f min\n", sec/60).."\n")
