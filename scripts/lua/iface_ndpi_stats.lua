756b4778c4046d0e9005a9353e88de82331dcd13
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-01-13 22:15:25 +0000

    Minor fix
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@7178 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/lua/iface_ndpi_stats.lua
+++ scripts/lua/iface_ndpi_stats.lua
@@ -19,57 +19,58 @@ else
    stats = interface.getHostInfo(_GET["host"])
 end
 
-tot = 0
-_ifstats = {}
-for key, value in pairs(stats["ndpi"]) do
-    --    print("->"..key.."\n")
-   traffic = stats["ndpi"][key]["bytes.sent"] + stats["ndpi"][key]["bytes.rcvd"]
-   _ifstats[traffic] = key
-   --print(key.."="..traffic)
-   tot = tot + traffic
-end
-
-
--- Print up to this number of entries
-max_num_entries = 5
-
--- Print entries whose value >= 5% of the total
-threshold = (tot * 3) / 100
-
-
 print "[\n"
-num = 0
-accumulate = 0
-for key, value in pairsByKeys(_ifstats, rev) do
-   if(key < threshold) then
-     break
+
+if(stats ~= nil) then
+   tot = 0
+   _ifstats = {}
+   for key, value in pairs(stats["ndpi"]) do
+      --    print("->"..key.."\n")
+      traffic = stats["ndpi"][key]["bytes.sent"] + stats["ndpi"][key]["bytes.rcvd"]
+      _ifstats[traffic] = key
+      --print(key.."="..traffic)
+      tot = tot + traffic
    end
 
-   if(num > 0) then
-      print ",\n"
+
+   -- Print up to this number of entries
+   max_num_entries = 5
+
+   -- Print entries whose value >= 5% of the total
+   threshold = (tot * 3) / 100
+
+   num = 0
+   accumulate = 0
+   for key, value in pairsByKeys(_ifstats, rev) do
+      if(key < threshold) then
+	 break
+      end
+
+      if(num > 0) then
+	 print ",\n"
+      end
+
+      print("\t { \"label\": \"" .. value .."\", \"value\": ".. key .." }")
+      accumulate = accumulate + key
+      num = num + 1
+
+      if(num == max_num_entries) then
+	 break
+      end
    end
 
-   print("\t { \"label\": \"" .. value .."\", \"value\": ".. key .." }")
-   accumulate = accumulate + key
-   num = num + 1
-
-   if(num == max_num_entries) then
-     break
-end
-end
-
-if(tot == 0) then
-   tot = 1
-end
-
--- In case there is some leftover do print it as "Other"
-if(accumulate < tot) then
-   if(num > 0) then
-      print (",\n")
+   if(tot == 0) then
+      tot = 1
    end
 
-   print("\t { \"label\": \"Other\", \"value\": ".. (tot-accumulate) .." }")
+   -- In case there is some leftover do print it as "Other"
+   if(accumulate < tot) then
+      if(num > 0) then
+	 print (",\n")
+      end
+
+      print("\t { \"label\": \"Other\", \"value\": ".. (tot-accumulate) .." }")
+   end
 end
 
 print "\n]"
-
