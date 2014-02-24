ef72743ca2c20a233c92e63b571ea66b6d6b000b
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-02-24 20:37:46 +0000

    Minor fix
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@7339 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/lua/network_load.lua
+++ scripts/lua/network_load.lua
@@ -9,15 +9,20 @@ require "lua_utils"
 
 interface.find(ifname)
 ifstats = interface.getStats()
-uptime = ntop.getUptime()
-prefs = ntop.getPrefs()
-
--- Round up
-hosts_pctg = math.floor(1+((ifstats.stats_hosts*100)/prefs.max_num_hosts))
-flows_pctg = math.floor(1+((ifstats.stats_flows*100)/prefs.max_num_flows))
-aggregations_pctg = math.floor(1+((ifstats.stats_aggregations*100)/prefs.max_num_hosts))
 
 sendHTTPHeader('text/html')
 --sendHTTPHeader('application/json')
 
-print('{ "packets": '.. ifstats.stats_packets .. ', "bytes": ' .. ifstats.stats_bytes .. ', "drops": ' .. ifstats.stats_drops .. ', "alerts": '.. ntop.getNumQueuedAlerts() ..', "num_flows": '.. ifstats.stats_flows .. ', "num_hosts": ' .. ifstats.stats_hosts .. ', "num_aggregations": ' .. ifstats.stats_aggregations .. ', "epoch": ' .. os.time()..' , "uptime": " ' .. secondsToTime(uptime) .. '", "hosts_pctg": ' .. hosts_pctg .. ', "aggregations_pctg": ' .. aggregations_pctg .. ', "flows_pctg": ' .. flows_pctg.. ' }\n')
+if(ifstats ~= nil) then
+   uptime = ntop.getUptime()
+   prefs = ntop.getPrefs()
+   
+-- Round up
+   hosts_pctg = math.floor(1+((ifstats.stats_hosts*100)/prefs.max_num_hosts))
+   flows_pctg = math.floor(1+((ifstats.stats_flows*100)/prefs.max_num_flows))
+   aggregations_pctg = math.floor(1+((ifstats.stats_aggregations*100)/prefs.max_num_hosts))
+   
+   print('{ "packets": '.. ifstats.stats_packets .. ', "bytes": ' .. ifstats.stats_bytes .. ', "drops": ' .. ifstats.stats_drops .. ', "alerts": '.. ntop.getNumQueuedAlerts() ..', "num_flows": '.. ifstats.stats_flows .. ', "num_hosts": ' .. ifstats.stats_hosts .. ', "num_aggregations": ' .. ifstats.stats_aggregations .. ', "epoch": ' .. os.time()..' , "uptime": " ' .. secondsToTime(uptime) .. '", "hosts_pctg": ' .. hosts_pctg .. ', "aggregations_pctg": ' .. aggregations_pctg .. ', "flows_pctg": ' .. flows_pctg.. ' }\n')
+else
+   print('{ }\n')
+end
