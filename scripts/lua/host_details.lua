e7b67acb232a96a7d9bfcb9ab2184b076d294467
fontanelli <fontanelli@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-03-07 10:57:40 +0000

    Remove markercluster minified
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@7353 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/lua/host_details.lua
+++ scripts/lua/host_details.lua
@@ -801,7 +801,7 @@ print [[
 
 </center>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-    <script src="/js/markerclusterer_compiled.js"></script>
+    <script src="/js/markerclusterer.js"></script>
 <div class="container-fluid">
   <div class="row-fluid">
     <div class="span8">
