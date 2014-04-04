d98cdc0c02b2548c4c594bfce8b9afe91919492e
fontanelli <fontanelli@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-04-04 17:25:44 +0000

    reworked code
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@7438 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- scripts/lua/host_sflow_distro.lua
+++ scripts/lua/host_sflow_distro.lua
@@ -11,13 +11,114 @@ local json = require ("dkjson")
 
 sendHTTPHeader('text/html')
 
-mode = _GET["mode"] -- user,process(proc_name) 
-type = _GET["type"] -- memory(actual-memory),bytes,latency
+
+local debug = false
+
+-----------------------------------
+
+function setAggregatedFlow(p_id,p_ip_address,p_value,p_what)
+  if (what_array[p_id] == nil) then 
+    what_array[p_id]  = {}
+    what_array[p_id]["value"]  = 0
+    what_array[p_id]["url"]  = url..p_what.."&host="..p_ip_address
+  end
+
+  if ((how_is_process == 1) or (how_is_latency == 1))then
+    if ( what_array[p_id]["value"]  == 0) then
+      what_array[p_id]["value"] = p_value
+    end
+  else
+    what_array[p_id]["value"] = what_array[p_id]["value"] + p_value
+  end
+end
+
+-----------------------------------
+
+function getAggretationValue(flow,flow_key,type)
+  l_how = 0;
+  process_key = "client_process"
+  bytes_key = "cli2srv.bytes"
+  
+  if (type == "server") then
+    process_key = "server_process"
+     bytes_key = "srv2cli.bytes"
+  end
+  
+  if (how_is_process == 1) then
+  
+    l_how = flow[process_key][how]
+  
+  elseif (how_is_latency == 1) then
+  
+    flow_more_info = interface.findFlowByKey(flow_key)
+    local info, pos, err = json.decode(flow_more_info["moreinfo.json"], 1, nil)
+    for k,v in pairs(info) do
+      if("Application latency (residual usec)" == getFlowKey(k)) then
+        l_how = tonumber(handleCustomFlowField(k, v))
+      end
+    end
+  
+  else
+  
+    l_how = flow[bytes_key]
+  
+  end
+  return l_how;
+end
+
+-----------------------------------
+
+function setType(p_type)
+  if((p_type == nil) or (p_type == "memory")) then
+    how = "actual_memory"
+    how_is_process = 1
+  elseif (p_type == "bytes") then
+    how = "bytes"
+  elseif (p_type == "latency") then
+    how_is_latency = 1
+    how = "Application latency (residual usec)"
+  end
+  
+  if (debug) then io.write("How:"..how.."\n"); end
+end
+
+-----------------------------------
+
+function setMode(p_mode)
+  if((p_mode == nil) or (p_mode == "user")) then
+    what = "user_name"
+    url = "/lua/get_user_info.lua?user="
+  elseif (p_mode == "process") then
+    what = "name"
+    url = "/lua/get_process_info.lua?name="
+  end
+  if (debug) then io.write("what:"..what..",url:"..url.."\n"); end
+end
+
+-----------------------------------
+
+function setFilter(p_filter)
+  if((p_filter == nil) or (p_filter == "All")) then
+    filter_client = 1
+    filter_server = 1
+  elseif (p_filter == "Client") then
+    filter_client = 1
+  elseif (p_filter == "Server") then
+    filter_server = 1
+  end
+  if (debug) then io.write("Client:"..filter_client..", Server:"..filter_server.."\n"); end
+end
+
+
+-----------------------------------
+
+mode = _GET["mode"] -- memory(actual-memory),bytes,latency
+type = _GET["type"] -- user,process(proc_name)
 host = _GET["host"]
 filter = _GET["filter"] -- all,client,server
 
 interface.find(ifname)
-local debug = false
+
 
 if(host == nil) then
   print("<div class=\"alert alert-error\"><img src=/img/warning.png> This flow cannot be found (expired ?)</div>")
@@ -30,35 +131,16 @@ else
   filter_server = 0
   how_is_process = 0
   how_is_latency = 0
+  url = ""
+  what = ""
+  how = ""
 
   -- Process parameter
-  if((type == nil) or (type == "memory")) then
-    how = "actual_memory"
-    how_is_process = 1
-  elseif (type == "bytes") then
-    how = "bytes"
-  elseif (type == "latency") then
-    how_is_latency = 1
-    how = "Application latency (residual usec)"
-  end
-  if (debug) then io.write("How:"..how.."\n"); end
-  if((mode == nil) or (mode == "user")) then
-    what = "user_name"
-    url = "/lua/get_user_info.lua?user="
-  elseif (mode == "process") then
-    what = "name"
-    url = "/lua/get_process_info.lua?name="
-  end
-
-  if((filter == nil) or (filter == "All")) then
-    filter_client = 1
-    filter_server = 1
-  elseif (filter == "Client") then
-    filter_client = 1
-  elseif (filter == "Server") then
-    filter_server = 1
-  end
-
+  setType(type)
+  setMode(mode)
+  setFilter(filter)
+  
+  -- scan flows
   tot = 0
   what_array = {}
   num = 0
@@ -80,81 +162,18 @@ else
     
     if ((client_process == 1))then
       current_what = flow["client_process"][what].." (client)"
-      if (how_is_process == 1) then
-        value = flow["client_process"][how] 
-      elseif (how_is_latency == 1) then
-        flow_more_info = interface.findFlowByKey(key)
-        local info, pos, err = json.decode(flow_more_info["moreinfo.json"], 1, nil)
-        for k,v in pairs(info) do
-          if("Application latency (residual usec)" == getFlowKey(k)) then
-            value = tonumber(handleCustomFlowField(k, v))
-            io.write("Client val:"..value.."-"..flow["client_process"][what]..'\n')
-          end
-        end
-      else
-        value = flow["cli2srv.bytes"]
-      end
       
-      if (what_array[current_what] == nil) then 
-        what_array[current_what]  = {}
-        what_array[current_what]["value"]  = 0
-        what_array[current_what]["url"]  = url..flow["client_process"][what].."&host="..flow["cli.ip"]
-      end
+      value = getAggretationValue(flow,key,"client")
+      setAggregatedFlow(current_what,flow["cli.ip"],value,flow["client_process"][what])
       
-       if ((how_is_process == 1) or (how_is_latency == 1))then
-        if ( what_array[current_what]["value"]  == 0) then
-          what_array[current_what]["value"] = value
-        end
-      else
-        what_array[current_what]["value"] = what_array[current_what]["value"] + value
-      end
-
       if (debug) then io.write("Find client_process:"..current_what..", Value:"..value..", Process:"..flow["client_process"]["name"]..",Pid:"..flow["client_process"]["pid"]..",Url:"..what_array[current_what]["url"].."\n"); end
-
     end
     
     if(server_process == 1) then
       current_what = flow["server_process"][what].." (server)"
       
-      if (how_is_process == 1) then
-       
-       value = flow["server_process"][how] 
-      
-      elseif (how_is_latency == 1) then
-      
-        flow_more_info = interface.findFlowByKey(key)
-      
-        local info, pos, err = json.decode(flow_more_info["moreinfo.json"], 1, nil)
-        for k,v in pairs(info) do
-          if("Application latency (residual usec)" == getFlowKey(k)) then
-            value = tonumber(handleCustomFlowField(k, v))
-            io.write("Server val:"..value.."-"..flow["server_process"][what]..'\n')
-          end
-        end
-      
-      else
-      
-        value = flow["srv2cli.bytes"]
-      
-      end
-      
-      if (what_array[current_what] == nil) then 
-      
-        what_array[current_what]  = {}
-        what_array[current_what]["value"]  = 0
-        what_array[current_what]["url"]  = url..flow["server_process"][what].."&host="..flow["srv.ip"]
-      
-      end
-      
-      if ((how_is_process == 1) or (how_is_latency == 1))then
-        if ( what_array[current_what]["value"]  == 0) then
-          what_array[current_what]["value"] = value
-        end
-      else
-        what_array[current_what]["value"] = what_array[current_what]["value"] + value
-      end
-      
-      tot = tot + value
+      value = getAggretationValue(flow,key,"server")
+      setAggregatedFlow(current_what,flow["srv.ip"],value,flow["server_process"][what])
       
       if (debug) then io.write("Find server_process:"..current_what..", Value:"..value..", Process:"..flow["server_process"]["name"]..",Pid:"..flow["server_process"]["pid"]..",Url:"..what_array[current_what]["url"].."\n"); end
 
--- scripts/lua/sflow_tree.lua
+++ scripts/lua/sflow_tree.lua
@@ -12,6 +12,9 @@ local json = require ("dkjson")
 
 sendHTTPHeader('text/html')
 
+local debug = false
+-----------------------------------
+
 function setAggregatedFlow(pid,father_pid,father_name,p_what,p_how,type)
  if (aggregated_flows[pid] == nil) then
     aggregated_flows[pid] = {};
@@ -29,197 +32,243 @@ function setAggregatedFlow(pid,father_pid,father_name,p_what,p_how,type)
   end
 end
 
+-----------------------------------
+
+function getAggretationValue(flow,flow_key,type)
+  l_how = 0;
+  process_key = "client_process"
+  bytes_key = "cli2srv.bytes"
+  
+  if (type == "server") then
+    process_key = "server_process"
+     bytes_key = "srv2cli.bytes"
+  end
+  
+  if (how_is_process == 1) then
+  
+    l_how = flow[process_key][how]
+  
+  elseif (how_is_latency == 1) then
+  
+    flow_more_info = interface.findFlowByKey(flow_key)
+    local info, pos, err = json.decode(flow_more_info["moreinfo.json"], 1, nil)
+    for k,v in pairs(info) do
+      if("Application latency (residual usec)" == getFlowKey(k)) then
+        l_how = tonumber(handleCustomFlowField(k, v))
+      end
+    end
+  
+  else
+  
+    l_how = flow[bytes_key]
+  
+  end
+  return l_how;
+end
+
+-----------------------------------
+
+function setType(p_type)
+  if((p_type == nil) or (p_type == "memory")) then
+    how = "actual_memory"
+    how_is_process = 1
+  elseif (p_type == "bytes") then
+    how = "bytes"
+  elseif (p_type == "latency") then
+    how_is_latency = 1
+    how = "Application latency (residual usec)"
+  end
+  
+  if (debug) then io.write("How:"..how.."\n"); end
+end
+
+-----------------------------------
+
+function setMode(p_mode)
+  if((p_mode == nil) or (p_mode == "process")) then
+    what = "name"
+    url = "/lua/get_process_info.lua?host="..host.."&pid="
+  elseif (p_mode == "user") then -- TODO
+    what = "user_name"
+     url = "/lua/get_user_info.lua?user="
+  end
+  if (debug) then io.write("what:"..what..",url:"..url.."\n"); end
+end
+
+-----------------------------------
+
+function setFilter(p_filter)
+  if((p_filter == nil) or (p_filter == "All")) then
+    filter_client = 1
+    filter_server = 1
+  elseif (p_filter == "Client") then
+    filter_client = 1
+  elseif (p_filter == "Server") then
+    filter_server = 1
+  end
+  if (debug) then io.write("Client:"..filter_client..", Server:"..filter_server.."\n"); end
+end
+
+-----------------------------------
+
 mode = _GET["mode"] -- memory(actual-memory),bytes,latency
 type = _GET["type"] -- user,process(proc_name)
 host = _GET["host"]
 filter = _GET["filter"] -- all,client,server
 
 interface.find(ifname)
-local debug = false
-
-flows_stats = interface.getFlowsInfo()
 
 
-aggregated_flows = {}
-father_process = {}
-cildren_proess = {}
-num = 0
-process_client = 0
-process_server = 0
-how_is_process = 0
-how_is_latency = 0
+if(host == nil) then
+  print("<div class=\"alert alert-error\"><img src=/img/warning.png> This flow cannot be found (expired ?)</div>")
+else
 
--- Process parameter
-  if((type == nil) or (type == "memory")) then
-    how = "actual_memory"
-    how_is_process = 1
-  elseif (type == "bytes") then
-    how = "bytes"
-  elseif (type == "latency") then
-    how_is_latency = 1
-    how = "Application latency (residual usec)"
-  end
-  
-  if((mode == nil) or (mode == "process")) then
-    what = "name"
-    url = "/lua/get_process_info.lua?host="..host.."&pid="
-  end
+  flows_stats = interface.getFlowsInfo()
 
-  if((filter == nil) or (filter == "All")) then
-    process_client = 1
-    process_server = 1
-  elseif (filter == "Client") then
-    process_client = 1
-  elseif (filter == "Server") then
-    process_server = 1
-  end
+  -- Default values
+  aggregated_flows = {}
+  father_process = {}
+  cildren_proess = {}
+  num = 0
+  filter_client = 0
+  filter_server = 0
+  how_is_process = 0
+  how_is_latency = 0
+  url = ""
+  what = ""
+  how = ""
 
-for key, value in pairs(flows_stats) do
-  flow = flows_stats[key]
-  process = 1
+  -- Process parameter
+  setType(type)
+  setMode(mode)
+  setFilter(filter)
 
-  if ((flow["cli.ip"] ~= host) and 
-    (flow["srv.ip"] ~= host)) then 
-    process = 0
-  end
+  -- Scan flows
+  for key, value in pairs(flows_stats) do
+    flow = flows_stats[key]
+    process = 1
 
-  if (process == 1) then
-
-    if (process_client == 1) and (flow["cli.ip"] == host) and (flow["client_process"] ~= nil) then 
-      client_id = flow["client_process"]["pid"]
-      if (how_is_process == 1) then
-        client_how = flow["client_process"][how]
-      elseif (how_is_latency == 1) then
-        flow_more_info = interface.findFlowByKey(key)
-        local info, pos, err = json.decode(flow_more_info["moreinfo.json"], 1, nil)
-        for k,v in pairs(info) do
-          if("Application latency (residual usec)" == getFlowKey(k)) then
-            client_how = tonumber(handleCustomFlowField(k, v))
-          end
-        end
-      else
-        client_how = flow["cli2srv.bytes"]
-      end
-      setAggregatedFlow(client_id,flow["client_process"]["father_pid"],flow["client_process"]["father_name"],flow["client_process"][what],client_how,"client")
-    end
-      
-    if (process_server == 1) and (flow["srv.ip"] == host) and (flow["server_process"] ~= nil) then 
-      server_id = flow["server_process"]["pid"]
-      if (how_is_process == 1) then
-        server_how = flow["server_process"][how]
-      elseif (how_is_latency == 1) then
-        flow_more_info = interface.findFlowByKey(key)
-        local info, pos, err = json.decode(flow_more_info["moreinfo.json"], 1, nil)
-        for k,v in pairs(info) do
-          if("Application latency (residual usec)" == getFlowKey(k)) then
-            server_how = tonumber(handleCustomFlowField(k, v))
-          end
-        end
-      else
-        server_how = flow["srv2cli.bytes"]
-      end
-      setAggregatedFlow(server_id,flow["server_process"]["father_pid"],flow["server_process"]["father_name"],flow["server_process"][what],server_how,"server")
+    if ((flow["cli.ip"] ~= host) and 
+      (flow["srv.ip"] ~= host)) then 
+      process = 0
     end
 
-  end
-end 
+    if (process == 1) then
 
-father_process = {}
-cildren_proess = {}
-num = 0
-tot = 0
-for key, value in pairs(aggregated_flows) do
-  flow = aggregated_flows[key]
-  -- print("Pid:"..flow["pid"]..", Father Pid:"..flow["father_pid"]..", Father Name:"..flow["father_name"]..", Type:"..flow["type"]..", What:"..flow[what]..", How:"..flow[how].."\n")
+      if (filter_client == 1) and (flow["cli.ip"] == host) and (flow["client_process"] ~= nil) then 
+        client_id = flow["client_process"]["pid"]
 
-  if(flow["pid"] == flow["father_pid"]) then
-    if(father_process[flow["father_pid"]] == nil) then
-      father_process[flow["father_pid"]] = {}
-      father_process[flow["father_pid"]]["name"] = flow[what]
-      father_process[flow["father_pid"]]["size"] = flow[how]
-      father_process[flow["father_pid"]]["pid"] = flow["pid"]      
+        client_how = getAggretationValue(flow,key,"client")
+        setAggregatedFlow(client_id,flow["client_process"]["father_pid"],flow["client_process"]["father_name"],flow["client_process"][what],client_how,"client")
+      end
+        
+      if (filter_server == 1) and (flow["srv.ip"] == host) and (flow["server_process"] ~= nil) then 
+        server_id = flow["server_process"]["pid"]
+
+        server_how = getAggretationValue(flow,key,"server")
+        setAggregatedFlow(server_id,flow["server_process"]["father_pid"],flow["server_process"]["father_name"],flow["server_process"][what],server_how,"server")
+      end
+
+    end
+  end 
+
+  father_process = {}
+  cildren_proess = {}
+  num = 0
+  tot = 0
+  for key, value in pairs(aggregated_flows) do
+    flow = aggregated_flows[key]
+    -- print("Pid:"..flow["pid"]..", Father Pid:"..flow["father_pid"]..", Father Name:"..flow["father_name"]..", Type:"..flow["type"]..", What:"..flow[what]..", How:"..flow[how].."\n")
+
+    if(flow["pid"] == flow["father_pid"]) then
+      if(father_process[flow["father_pid"]] == nil) then
+        father_process[flow["father_pid"]] = {}
+        father_process[flow["father_pid"]]["name"] = flow[what]
+        father_process[flow["father_pid"]]["size"] = flow[how]
+        father_process[flow["father_pid"]]["pid"] = flow["pid"]      
+      else
+        father_process[flow["father_pid"]]["size"] = father_process[flow["father_pid"]]["size"] + flow[how]
+      end
+      tot = tot + father_process[flow["father_pid"]]["size"]
     else
-      father_process[flow["father_pid"]]["size"] = father_process[flow["father_pid"]]["size"] + flow[how]
+      if(father_process[flow["father_pid"]] == nil) then
+        father_process[flow["father_pid"]] = {}
+        father_process[flow["father_pid"]]["children"] = {}
+        father_process[flow["father_pid"]]["children"][flow["pid"]] = {}
+        father_process[flow["father_pid"]]["children"][flow["pid"]]["name"] = flow[what]
+        father_process[flow["father_pid"]]["children"][flow["pid"]]["size"] = flow[how]
+        father_process[flow["father_pid"]]["pid"] = flow["pid"]
+        father_process[flow["father_pid"]]["size"] = flow[how]
+        father_process[flow["father_pid"]]["name"] = flow["father_name"]
+      else
+        father_process[flow["father_pid"]]["children"][flow["pid"]] = {}
+        father_process[flow["father_pid"]]["children"][flow["pid"]]["name"] = flow[what]
+        father_process[flow["father_pid"]]["children"][flow["pid"]]["size"] = flow[how]
+        father_process[flow["father_pid"]]["size"] = father_process[flow["father_pid"]]["size"] + flow[how]
     end
     tot = tot + father_process[flow["father_pid"]]["size"]
-  else
-    if(father_process[flow["father_pid"]] == nil) then
-      father_process[flow["father_pid"]] = {}
-      father_process[flow["father_pid"]]["children"] = {}
-      father_process[flow["father_pid"]]["children"][flow["pid"]] = {}
-      father_process[flow["father_pid"]]["children"][flow["pid"]]["name"] = flow[what]
-      father_process[flow["father_pid"]]["children"][flow["pid"]]["size"] = flow[how]
-      father_process[flow["father_pid"]]["pid"] = flow["pid"]
-      father_process[flow["father_pid"]]["size"] = flow[how]
-      father_process[flow["father_pid"]]["name"] = flow["father_name"]
-    else
-      father_process[flow["father_pid"]]["children"][flow["pid"]] = {}
-      father_process[flow["father_pid"]]["children"][flow["pid"]]["name"] = flow[what]
-      father_process[flow["father_pid"]]["children"][flow["pid"]]["size"] = flow[how]
-      father_process[flow["father_pid"]]["size"] = father_process[flow["father_pid"]]["size"] + flow[how]
+
   end
-  tot = tot + father_process[flow["father_pid"]]["size"]
 
-end
+  end
 
-end
+  if (debug) then 
+    for key, size in pairs(father_process) do
+      flow = father_process[key]
+      io.write("Father => Name:"..flow["name"]..", size:"..flow["size"]..", pid:"..flow["pid"].."\n")
+      if (flow["children"] ~= nil) then
+        for key, size in pairs(flow["children"]) do
+          children = flow["children"][key]
+          io.write("Children => Name:"..children["name"]..", size:"..children["size"]..", pid:"..flow["pid"].."\n")
+        end
+      end
+    end
+  end
 
-if (debug) then 
-  for key, size in pairs(father_process) do
+
+  print "{\n"
+  num = 0
+  s = 0
+  print("\"name\": \"" .. what .."\", \"size\": ".. tot ..", \n\"children\": [\n") 
+
+  for key, value in pairs(father_process) do
     flow = father_process[key]
-    io.write("Father => Name:"..flow["name"]..", size:"..flow["size"]..", pid:"..flow["pid"].."\n")
+    if(num > 0) then
+     print ",\n"
+    end
+    if (flow["name"] == "") then
+      if(debug) then io.write("Empty name\n") end
+      flow["name"] = "Empty name"
+    end
+    print("\t { \"name\": \"" .. flow["name"] .."\", \"id\": " .. key ..", \"size\": ".. flow["size"]..",\"url\": \"" .. url..key.."\"") 
+    
+    children_num = 0
     if (flow["children"] ~= nil) then
-      for key, size in pairs(flow["children"]) do
-        children = flow["children"][key]
-        io.write("Children => Name:"..children["name"]..", size:"..children["size"]..", pid:"..flow["pid"].."\n")
+      print( ", \n\t\"children\": \n\t\t[\n")
+      for k,v in pairs(flow["children"]) do
+        children = flow["children"][k]
+        if(children_num > 0) then
+          print ",\n"
+        end
+        if (children["name"] == "") then
+          if(debug) then io.write("Empty name children\n") end
+          children["name"] = "Empty name"
+        end
+        print("\t\t { \"name\": \"" .. children["name"] .."\", \"id\": ".. k ..", \"size\": ".. children["size"] ..",\"url\": \"" .. url..k.."\" }") 
+      
+        children_num = children_num + 1
+      
       end
+      print ("\n\t\t]\n")
     end
-  end
-end
 
-
-print "{\n"
-num = 0
-s = 0
-print("\"name\": \"" .. what .."\", \"size\": ".. tot ..", \n\"children\": [\n") 
-
-for key, value in pairs(father_process) do
-  flow = father_process[key]
-  if(num > 0) then
-   print ",\n"
-  end
-  if (flow["name"] == "") then
-    if(debug) then io.write("Empty name\n") end
-    flow["name"] = "Empty name"
-  end
-  print("\t { \"name\": \"" .. flow["name"] .."\", \"id\": " .. key ..", \"size\": ".. flow["size"]..",\"url\": \"" .. url..key.."\"") 
-  
-  children_num = 0
-  if (flow["children"] ~= nil) then
-    print( ", \n\t\"children\": \n\t\t[\n")
-    for k,v in pairs(flow["children"]) do
-      children = flow["children"][k]
-      if(children_num > 0) then
-        print ",\n"
-      end
-      if (children["name"] == "") then
-        if(debug) then io.write("Empty name children\n") end
-        children["name"] = "Empty name"
-      end
-      print("\t\t { \"name\": \"" .. children["name"] .."\", \"id\": ".. k ..", \"size\": ".. children["size"] ..",\"url\": \"" .. url..k.."\" }") 
+    print ("\t}")
     
-      children_num = children_num + 1
-    
-    end
-    print ("\n\t\t]\n")
+   num = num + 1
+
   end
 
-  print ("\t}")
-  
- num = num + 1
 
-end
+  print "]\n}"
 
-
-print "]\n}"
-
+end -- if host error
