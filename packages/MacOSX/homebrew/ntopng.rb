e8c6b16c9f8f49f67d91320fcf6b88abf07d13e6
fontanelli <fontanelli@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2014-07-19 15:07:07 +0000

    Minor changes
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntopng@7926 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- packages/MacOSX/homebrew/ntopng.rb
+++ packages/MacOSX/homebrew/ntopng.rb
@@ -2,36 +2,37 @@ require "formula"
 
 class Ntopng < Formula
   homepage "http://www.ntop.org"
-  url 'https://svn.ntop.org/svn/ntop/trunk/ntopng/', :tag => '7829'
+  url 'https://svn.ntop.org/svn/ntop/tags/ntopng-1.1.4/'
   version "1.1.4"
 
   devel do
-     url 'https://svn.ntop.org/svn/ntop/trunk/ntopng/', :tag => '7830'
-     version "1.1.4-7830"
+     url 'https://svn.ntop.org/svn/ntop/trunk/ntopng/'
+     version "1.1.4"
   end
 
   depends_on :autoconf => :build
   depends_on :automake => :build
   depends_on 'pkg-config' => :build
   depends_on 'json-glib' => :build
+  depends_on 'json-c' => :build
   depends_on :libtool => :build
   depends_on 'sqlite'
   depends_on 'rrdtool'
   depends_on 'wget'
-  depends_on 'libxml2'
   depends_on 'redis'
   depends_on 'glib'
-  depends_on 'lbzip2'
   depends_on 'zeromq'
 
   def install
     system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
 
   test do
-    system "true"
+    # tests if it can load chisels
+    `#{bin}/ntopng -h`
+    assert_equal 0, $?.exitstatus
   end
 end
